38352b1409a19ed1bbc7a1c02544ab53bb80be57
Jake Garcia <bballwiz4@gmail.com>
2023-03-21 17:29:22 -0700

    Added in presence validators for the match model properties of game_duration, winning_user_id and losing_user_id. Also created a custom validator to check that the id's being used for winning_user_id and losing_user_id are associated with valid users.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,20 @@
-class Match < ApplicationRecord
+class MatchValidator < ActiveModel::Validator
+    def validate(record)
+        if User.find_by(id: record.winning_user_id) == false
+            record.errors.add :winning_user_id, "Winning user id must be associated with a valid user."
+        end
+        if User.find_by(id: record.losing_user_id) == false
+            record.errors.add :losing_user_id, "Losing user id must be associated with a valid user."
+        end
+    end
 end
+
+
+class Match < ApplicationRecord
+    include ActiveModel::Validations
+    validates :game_duration, presence: true, numericality: {greater_than: 1}
+    validates :winning_user_id, presence: true
+    validates :losing_user_id, presence: true
+    validates_with MatchValidator
+end
+
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,13 +1,27 @@
 
 RSpec.describe Match do
+    before(:all) do
+        @winning_user = User.create(email: "test1@gmail.com", username: "testUser1", password: "Password")
+        @losing_user = User.create(email: "test2@gmail.com", username: "testUser2", password: "Password")
+    end
     it "raises an error if it does not have a game_duration greater than 0" do
+        match = Match.create(winning_user_id: @winning_user.id, losing_user_id: @losing_user.id, game_duration: 0)
+        expect(match.valid?).to eq(false)
     end
     it "raises an error if no winning user id is present" do 
+        match = Match.create(losing_user_id: @losing_user.id, game_duration: 0)
+        expect(match.valid?).to eq(false)
     end
     it "raises an error if a valid winning user id is not present" do
+        match = Match.create(winning_user_id: 100, losing_user_id: @losing_user.id, game_duration: 0)
+        expect(match.valid?).to eq(false)
     end
     it "raises an error if no losing user id is present" do 
+        match = Match.create(winning_user_id: @winning_user.id, game_duration: 0)
+        expect(match.valid?).to eq(false)
     end
     it "raises an error if a valid losing user id is not present" do
+        match = Match.create(winning_user_id: @winning_user.id, losing_user_id: 100, game_duration: 0)
+        expect(match.valid?).to eq(false)
     end
 end
