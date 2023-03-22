a7218de8dd69bcb18614606587f184f51ef6812a
Jake Garcia <bballwiz4@gmail.com>
2023-03-21 17:09:30 -0700

    Wrote out 5 test cases for a match model.

---
+++ spec/models/match_spec.rb
@@ -0,0 +1,13 @@
+
+RSpec.describe Match do
+    it "raises an error if it does not have a game_duration greater than 0" do
+    end
+    it "raises an error if no winning user id is present" do 
+    end
+    it "raises an error if a valid winning user id is not present" do
+    end
+    it "raises an error if no losing user id is present" do 
+    end
+    it "raises an error if a valid losing user id is not present" do
+    end
+end
