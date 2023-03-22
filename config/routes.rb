20c0d8eb9aab8b4107d9896a6ad915d3adadc690
Jake Garcia <bballwiz4@gmail.com>
2023-03-22 12:23:30 -0700

    Added a route to point to the users controller create action. Added in logic to handle sending back a json response to the frontend whether a user is created successfully or not. Added in a private method user_params that requires the user information to be sent in a hash with a key of user_info and the properties permitted are :email, :username, :password. This route is if the user decides to enter in their information rather than using a oAuth.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#welcome"
+  post '/users', to: "users#create"
 end
