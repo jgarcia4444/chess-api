3972bd38d768d204327d403af14017f39b13a459
Jake Garcia <bballwiz4@gmail.com>
2023-03-23 17:58:47 -0700

    Fixed an error appending .com to the end of the email when searching for a saveed user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,38 @@
 
-
-def login
-    if params[:login_info]
-        login_info = params[:login_info]
-        if login_info[:email]
-            email = login_info[:email]
-            user = User.find_by(email:  "#{email}.com")
-            if user 
-                if login_info[:password]
-                    password = login_info[:password]
-                    if user.authenticate(password)
-                        render :json => {
-                            success: true,
-                            user_info: {
-                                id: user.id,
-                                username: user.username,
-                                email: user.email,
+class SessionsController < ApplicationController
+    def login
+        if params[:login_info]
+            login_info = params[:login_info]
+            if login_info[:email]
+                email = login_info[:email]
+                user = User.find_by(email: email)
+                if user 
+                    if login_info[:password]
+                        password = login_info[:password]
+                        if user.authenticate(password)
+                            render :json => {
+                                success: true,
+                                user_info: {
+                                    id: user.id,
+                                    username: user.username,
+                                    email: user.email,
+                                }
                             }
-                        }
+                        else
+                            render :json => {
+                                success: false,
+                                error: {
+                                    message: "",
+                                    errors: [{type: "PASSWORD", message: "Incorrect password"}]
+                                }
+                            }
+                        end
                     else
                         render :json => {
                             success: false,
                             error: {
-                                message: "",
-                                errors: [{type: "PASSWORD", message: "Incorrect password"}]
+                                message: "Information sent improperly.",
+                                errors: [{type: "PASSWORD", message: "Password is required to login."}]
                             }
                         }
                     end
@@ -31,8 +40,8 @@ def login
                     render :json => {
                         success: false,
                         error: {
-                            message: "Information sent improperly.",
-                            errors: [{type: "PASSWORD", message: "Password is required to login."}]
+                            message: "",
+                            errors: [{type: "EMAIL", message: "A user was not found with the given email."}]
                         }
                     }
                 end
@@ -40,8 +49,8 @@ def login
                 render :json => {
                     success: false,
                     error: {
-                        message: "",
-                        errors: [{type: "EMAIL", message: "A user was not found with the given email."}]
+                        message: "Information was sent improperly",
+                        errors: [{type: "EMAIL", message: "An email is required to login"}],
                     }
                 }
             end
@@ -50,18 +59,10 @@ def login
                 success: false,
                 error: {
                     message: "Information was sent improperly",
-                    errors: [{type: "EMAIL", message: "An email is required to login"}],
+                    errors: []
                 }
             }
         end
-    else
-        render :json => {
-            success: false,
-            error: {
-                message: "Information was sent improperly",
-                errors: []
-            }
-        }
+    
     end
-
 end
