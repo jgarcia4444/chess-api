88a4beb3478a79d8b754dfbae39ab1d716c03437
Jake Garcia <bballwiz4@gmail.com>
2023-03-26 21:03:48 -0700

    Got devise auth tokens set up and installed.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#welcome"
-  post '/users', to: "users#create"
+  post '/users/manual', to: "users#create"
   post '/users/login', to: "sessions#login"
 
-  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  # mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   post 'google_auth/callback', to: "google_auth#authenticate_social_auth_user"
 
 end
